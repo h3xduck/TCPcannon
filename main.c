2a8662950ba9ab098bfc9a4f39e0120769461f5a
MARCOS SANCHEZ BAJO <100405823@alumnos.uc3m.es>
2021-03-31 16:42:49 +0200

    Added license

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 
 int main(){
 
-    //syn_flooding_spoofed("192.168.1.1");
-    benchmark_syn_flooding_spoofed("192.168.1.1");
+    syn_flooding_direct("192.168.1.1");
+    //benchmark_syn_flooding_spoofed("192.168.1.1");
 
     return 0;
 }
